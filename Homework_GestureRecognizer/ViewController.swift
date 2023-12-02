e19d50178cdc27631a71a73455300c46d7b11d5e
Vlad Krupen <air7360@gmail.com>
2023-12-02 15:15:53 +0300

    added two identical views and a button
    when the view intersects, the movement stops and a button appears, which, when pressed, returns the view to its original place

--- Homework_GestureRecognizer/ViewController.swift
+++ Homework_GestureRecognizer/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movingView: UIView!
+    
+    @IBOutlet weak var staticView: UIView!
+    
+    @IBOutlet weak var startAgain: UIButton!
+    
+    var originalPointMovingView: CGRect!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        movingView.layer.cornerRadius = movingView.frame.height / 2
+//        staticView.layer.cornerRadius = staticView.frame.height / 2
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureMoved))
+        
+        movingView.addGestureRecognizer(gestureRecognizer)
+        originalPointMovingView = self.movingView.frame
+        
+        startAgain.isHidden = true
+
+    }
+    
+    @objc func panGestureMoved(_ sender: UIPanGestureRecognizer) {
+        let point = sender.translation(in: view)
+        
+        movingView.center = CGPoint(x: movingView.center.x + point.x,
+                                    y: movingView.center.y + point.y)
+        
+        sender.setTranslation(.zero, in: view)
+        
+        if movingView.frame.intersects(staticView.frame) == true {
+            sender.state = .ended
+            startAgain.isHidden = false
+        }
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        movingView.frame = originalPointMovingView
+        startAgain.isHidden = true
+    }
+    
 }
 
